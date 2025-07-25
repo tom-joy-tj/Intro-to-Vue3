451f31d1e9124be9c3f0150833ced4b2823289f9
Thomas Joy <thomaspjoy1@gmail.com>
2025-07-25 10:35:45 +0100

    completed lesson 5

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 1, colour: 'green' },
+                { id: 2, colour: 'blue' },
+            ],
+            sizes: ['Small', 'Medium', 'Large'],
         }
     }
 })
